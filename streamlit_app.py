5aa47f71585331565a2ea7d4c6426a95e44e8051
Harshitha Bandepureddy <bandepureddyharshitha@gmail.com>
2025-04-03 17:43:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,6 @@ st.markdown(
 )
 
 tv = st.slider('What is price for given TV advertisement budget listing ?', 0, 300, 150)
-# Predict sales based on the input
-user_input = np.array([[tv]]) 
-prediction = model.predict(user_input)[0]
-
-# Display the prediction using a bar chart
-st.bar_chart(data=prediction, width=200, height=200, use_container_width=True)
 
 user_input = [[tv]] #, newspaper, radio]]
 #user_input = user_input.astype(float)
